b04b983d8245c15f8c60e331933917934e082334
KongMy <kuribu99@hotmail.com>
2016-06-24 11:11:26 +0800

    Removed unused code from api.js

--- models.js
+++ models.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 var _ = require('underscore');
 
 module.exports = function (wagner) {
-    mongoose.connect('mongodb://localhost:27017/srs_tool');
+    //mongoose.connect('mongodb://localhost:27017/srs_tool');
+    mongoose.connect('mongodb://admin:kuribu99@ds019624.mlab.com:19624/srs_tool');
 
     wagner.factory('db', function () {
         return mongoose;
