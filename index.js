260d29622a6a670b3684d0c2506cfefc2d7028b9
KongMy <kuribu99@hotmail.com>
2016-06-24 11:51:49 +0800

    Changed files to CDN and use env.port

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ require('./dependencies')(wagner);
 
 var app = express();
 
-app.use(require('morgan')('[:method] :url'));
+//app.use(require('morgan')('[:method] :url'));
 
 //wagner.invoke(require('./auth'), { app: app });
 
 app.use('/api/v1', require('./routes/api')(wagner));
-app.use(express.static('./public'/*, { maxAge: 4 * 60 * 60 * 1000 }*/));
+app.use(express.static('./public', { maxAge: 4 * 60 * 60 * 1000 }));
 
 app.listen(wagner.invoke(function (Config) {
-    console.log('Listening on port ' + Config.port + '!');
+	var port = process.env.PORT || Config.port || 3000;
+    console.log('Listening on port ' + port + '!');
     return Config.port;
 }));
