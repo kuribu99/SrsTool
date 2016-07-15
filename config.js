ba131589cdbd2d5c637c31d9d4de32789926c826
KongMy <kuribu99@hotmail.com>
2016-07-15 15:32:49 +0800

    Updated website url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = function () {
     return {
-        WebsiteURL: 'http://localhost:3000/',
+        WebsiteURL: 'http://srs-tool.herokuapp.com/',
         FacebookAppID: '194195624316925',
         FacebookAppSecret: '41482af6ae9b160468496fc17155664c'
     };
